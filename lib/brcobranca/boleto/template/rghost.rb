c831b2cf16fd79432197ec994c6082e0b4b7d249
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2014-01-13 10:02:02 -0200

    Update rghost.rb
    uncomment

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -201,17 +201,16 @@ module Brcobranca
         end
 
         # Monta o corpo e rodapé do layout do boleto
-        def modelo_generico_rodape(doc, boleto) #, opts = {:grande => [:grande], :pos_y => 16.8 }
+        def modelo_generico_rodape(doc, boleto, opts = {:grande => :grande, :logo => 80})
           #INICIO Segunda parte do BOLETO BB
           # LOGOTIPO do BANCO
-          #doc.text_area cedente, :width => '8.5 cm', :x => '0.7 cm' , :y => '19.5 cm'
-
-          #doc.image(boleto.logotipo, :x => '0.5 cm', :y => opts[:pos_y].to_s + ' cm', :zoom => 80)
-          #doc.moveto :x => '5.2 cm' , :y => opts[:pos_y].to_s + ' cm'
-          #doc.show "#{boleto.banco}-#{boleto.banco_dv}", :tag => opts[:grande]
-          #doc.moveto :x => '7.5 cm' , :y => opts[:pos_y].to_s + ' cm'
-          #doc.show boleto.codigo_barras.linha_digitavel, :tag => opts[:grande]
-          #doc.moveto :x => '0.7 cm' , :y => opts[:pos_y].to_s + ' cm'
+          doc.text_area cedente, :width => '8.5 cm', :x => '0.7 cm' , :y => '19.5 cm'
+          doc.image(boleto.logotipo, :x => '0.5 cm', :y => '16.8 cm', :zoom => opts[:logo])
+          doc.moveto :x => '5.2 cm' , :y => '16.8 cm'
+          doc.show "#{boleto.banco}-#{boleto.banco_dv}", :tag => opts[:grande]
+          doc.moveto :x => '7.5 cm' , :y =>  '16.8 cm'
+          doc.show boleto.codigo_barras.linha_digitavel, :tag => opts[:grande]
+          doc.moveto :x => '0.7 cm' , :y => '16.8 cm'
           doc.show boleto.local_pagamento
           doc.moveto :x => '0.7 cm' , :y => '11.2 cm'
           doc.show boleto.cedente
