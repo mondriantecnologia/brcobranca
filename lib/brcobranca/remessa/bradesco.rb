56844c211043fe9df0827342bf75bbc5074720a0
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2014-02-18 12:32:54 -0200

    Update bradesco.rb

--- lib/brcobranca/remessa/bradesco.rb
+++ lib/brcobranca/remessa/bradesco.rb
@@ -13,10 +13,15 @@ module Brcobranca
       def gerar_arquivo
         @arquivo = File.open(self.caminho_arquivo, "w")
         
-        primeira_linha = self.cabecalho_generico
-        @arquivo.write(primeira_linha)
-        
-        if self.objeto.class == PagamentoEstorno
+        if self.objeto.class == ArquivoRemessa
+          primeira_linha = self.cabecalho_arquivo_remessa
+          @arquivo.write(primeira_linha)
+          numero_de_registros = self.corpo_arquivo_remessa
+          rodape = self.rodape_arquivo_remessa(numero_de_registros)
+          @arquivo.write(rodape)
+        elsif self.objeto.class == PagamentoEstorno
+          primeira_linha = self.cabecalho_pagamento_estorno
+          @arquivo.write(primeira_linha)
           numero_de_registros = self.corpo_pagamento_estorno
           rodape = self.rodape_pagamento_estorno(numero_de_registros)
           @arquivo.write(rodape)
@@ -24,11 +29,32 @@ module Brcobranca
         
         @arquivo.close
 
-        comando = "sed -i 's/$/\\r/' #{nome_arquivo}"    
+        comando = "sed -i 's/$/\\r/' #{self.caminho_arquivo}"    
         system(comando)
       end
       
-      def cabecalho_generico
+      def cabecalho_arquivo_remessa
+        primeira_linha = "01" # 1 a 2 identificaçao do registro e identificaçao do arquivo remessa
+        primeira_linha << "REMESSA" # 3 a 9 literal
+        primeira_linha << "01" # 10 a 11 Código de Serviço
+        primeira_linha << "COBRANCA".ljust(15," ") # 12 a 26 
+        primeira_linha << self.parametros.cod_cliente.rjust(20,"0") # 27 a 43 codigo da empresa
+        primeira_linha << I18n.transliterate(self.parametros.cedente)[0,30].ljust(30," ").upcase # 44 a 76 nome da empresa
+        primeira_linha << "237".ljust(3," ")  # 77 a 79 numero do bradesco
+        primeira_linha << "BRADESCO".ljust(15," ") # 80 a 94 bradesco
+        primeira_linha << Time.now.strftime('%d%m%y').to_s.rjust(6,"0") # 95 a 100 data de gravaçao do arquivo
+        primeira_linha << "".ljust(8," ") # 101 a 108 branco
+        primeira_linha << "MX".ljust(2," ") # 109 a 110 identificação do sistema 
+        primeira_linha << self.parametros.sequencial_arquivo_remessa.to_s.rjust(7,"0")   # 111 a 117 numero sequencial de remessa 
+        primeira_linha << "".ljust(277," ") # 118 a 394 branco
+        primeira_linha << num_sequencia.to_s.rjust(6,"0") # 395 a 400 numero sequencial do registro de um em um 
+        primeira_linha << "\n"
+        self.parametros.sequencial_arquivo_remessa += 1
+        self.parametros.save 
+        return primeira_linha
+      end
+
+      def cabecalho_pagamento_estorno
         primeira_linha =  "0" #001 A 001 - Identificação do Registro - 001 -Obrigatório – fixo “zero”(0)
         primeira_linha << self.parametros.codigo_de_comunicacao.to_s.rjust(8,'0') # 002 A 009 - Código de Comunicação - 008 
         primeira_linha << '2' #010 A 010 -  Tipo de Inscrição da Empresa Pagadora - 1 = CPF / 2 = CNPJ / 3= OUTROS
@@ -57,6 +83,65 @@ module Brcobranca
         return primeira_linha
       end
       
+      def corpo_arquivo_remessa
+        numero_de_registros = self.num_sequencia
+        self.boletos.each do |boleto|
+          numero_de_registros += 1
+          linha = "1"
+          linha << "00000"   # 2 a 6 agencia debito 
+          linha << " "       # 7 a 7 digito agencia debito
+          linha << "00000"   # 8 a 12 razão conta corrente
+          linha << "0000000" # 13 a 19 conta corrente
+          linha << " "       # 20 a 20 digito
+          linha << "0"       # 21 a 21 zero
+          linha << self.parametros.carteira_financeira.to_s.rjust(3,"0") # 22 a 24 codigo da carteira
+          linha << self.parametros.agencia.rjust(5,"0")  # 25 a 29 agencia cedente sem digito
+          linha << self.parametros.conta.rjust(7,"0")    # 30 a 36 conta corrente
+          linha << self.parametros.conta_dv.to_s # 37 a 37 digito conta
+          linha << "".rjust(25," ")      # 38 a 62 uso da empresa
+          linha << "".rjust(3,"0")       # 63 a 65 codigo banco a ser debitado
+          linha << "2".rjust(1,"0")      # 66 a 66 campo de multa
+          linha << "0200".rjust(4,"0")   # 67 a 70 percentual de multa a ser considerado
+          linha << boleto.id.to_s.rjust(11,"0")         # 71 a 81 identificação do titulo "tem que ser gerado automatico"
+          linha << boleto.digito_verificador(self.parametros.carteira_financeira.to_s) # 82 a 82 digito de auto conferencia "tem que ser gerado automatico"
+          linha << "".rjust(10,"0") # 83 a 92 desconto bonificação
+          linha << "2" # 93 a 93 condição para emissão da papeleta
+          linha << "N" # 94 a 94 identificação se emite papeleta para debito
+          linha << "".ljust(10," ") # 95 a 104 identificação da operação do banco
+          linha << " " # 105 a 105 indicador rateio
+          linha << " " # 106 a 106 endereço para aviso
+          linha << " ".ljust(2," ") # 107 a 108 branco
+          linha << "01" # 109 a 110 identificacao da ocorrencia
+          linha << boleto.id.to_s.ljust(10," ") # 111 a 120 num do documento "tem que ser gerado automatico"
+          linha << boleto.vencimento.strftime('%d%m%y').rjust(6,"0")  # 121 a 126 data do vencimento do titulo
+          linha << boleto.valor.contabil.gsub('.','').gsub(',','').rjust(13,"0") # 127 a 139 valor do titulo
+          linha << "".rjust(3,"0")  # 140 a 142 banco encarregado da cobrança
+          linha << "".rjust(5,"0")  # 143 a 147 agencia depositaria
+          linha << "01" # 148 a 149 especie de titulo
+          linha << "N"  # 150 a 150 identificação
+          linha << boleto.created_at.strftime('%d%m%y').rjust(6,"0")  # 151 a 156 data da emissão do titulo "falta preencher"
+          linha << "".rjust(2,"0")  # 157 a 158 1a instrução
+          linha << "".rjust(2,"0")  # 159 a 160 2a instrução
+          linha << (boleto.valor / 3000.0).contabil.gsub('.','').gsub(',','').rjust(13,"0") # 161 a 173 valor da cobrança por dia atraso "verificar o pecentual de cobrança"
+          linha << "".rjust(6,"0")  # 174 a 179 Data Limite P/Concessão de Desconto
+          linha << "".rjust(13,"0") # 180 a 192 Valor do Desconto
+          linha << "".rjust(13,"0") # 193 a 205 Valor do IOF
+          linha << "".rjust(13,"0") # 206 a 218 Valor do Abatimento a ser concedido ou cancelado
+          linha << "02".rjust(2,"0")  # 219 a 220 identificação do tipo de incrição do sacado
+          linha << boleto.fatura.cartorio.cnpj.gsub('.','').gsub('-','').gsub('/','').rjust(14,"0") # 221 a 234 incriçao do sacado
+          linha << retira_acentos(boleto.fatura.cartorio.oficio)[0,40].ljust(40," ") # 235 a 274 nome do sacado
+          linha << "".ljust(40," ") # 275 a 314 end completo
+          linha << "".ljust(12," ") # 315 a 326 1a menssagem
+          linha << "".rjust(5,"0")  # 327 a 331 CEP
+          linha << "".rjust(3,"0")  # 332 a 334 sufixo do CEP
+          linha << "".ljust(60," ") # 335 a 394 sacador ou 2a menssagem
+          linha << numero_de_registros.to_s.rjust(6,"0")  # 395 a 400 num sequencial do registro
+          linha << "\n"
+          @arquivo.write(linha)
+        end
+        return numero_de_registros
+      end
+
       def corpo_pagamento_estorno
         numero_de_registros = self.num_sequencia
         self.objeto.ressarcimentos.each do |ressarcimento|
@@ -126,6 +211,15 @@ module Brcobranca
         return numero_de_registros
       end
     
+      def rodape_arquivo_remessa(numero_de_registros)
+        numero_de_registros += 1
+        linha_rodape = "9" # identificacao do registro
+        linha_rodape << "".ljust(393," ") # branco
+        linha_rodape << numero_de_registros.to_s.rjust(6,"0") # 395 a 400 numero sequencial do registro de um em um 
+        linha_rodape << "\n"
+        return linha_rodape
+      end
+
       def rodape_pagamento_estorno(numero_de_registros)
         numero_de_registros += 1
         linha_rodape = '9' # 001 A 001 - fixo 9
@@ -136,7 +230,11 @@ module Brcobranca
         linha_rodape << "\n"
         return linha_rodape        
       end
-    
+      
+      def retira_acentos(texto) 
+        retorno = texto.upcase.gsub('º','o').gsub('ª','a').gsub('Á','A').gsub('À','A').gsub('Ã','A').gsub('Â','A').gsub('É','E').gsub('È','E').gsub('Ê','E').gsub('Í','I').gsub('Ì','I').gsub('Î','I').gsub('Ó','O').gsub('Ò','O').gsub('Ô','O').gsub('Õ','O').gsub('Ú','U').gsub('Ù','U').gsub('Û','U').gsub('Ü','U').gsub('Ç','C').gsub(' ','o ')
+      end
+
     end
   end
 end
