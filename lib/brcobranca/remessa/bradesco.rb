0a705d46ba7a8c6bd76e18a6304b1e8dda449248
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2015-01-20 13:59:38 -0300

    corpo_arquivo_repasse

--- lib/brcobranca/remessa/bradesco.rb
+++ lib/brcobranca/remessa/bradesco.rb
@@ -245,6 +245,76 @@ module Brcobranca
         end
         return numero_de_registros
       end
+
+      def corpo_arquivo_repasse
+        numero_de_registros = self.num_sequencia
+        self.objeto.itens.each do |item_repasse|
+          if item_repasse.valor >= 0.01
+            numero_de_registros += 1
+            informacoes_complementares = (item_repasse.cartorio.dados_cartorario.codigo_banco == '237' ? ''.ljust(40,' ') : "C000000010#{item_repasse.cartorio.dados_cartorario.tipo_conta}".ljust(40,' ') )
+            ## trata formatacao do cpf e cnpj para arquivo
+            vcpf = item_repasse.cartorio.dados_cartorario.cpf_cnpj.gsub(/[^0-9]/,'')
+            if vcpf.size == 11
+              vcpf = vcpf[0,9] + '0000' + vcpf[9,2]
+            end   
+            linha = "1"
+            linha << item_repasse.cartorio.dados_cartorario.tipo_de_inscricao.to_s # 002 A 002 - Tipo de inscricao do Fornecedor - 1- CPF, 2- CNPJ, 3-OUtros
+            linha << vcpf.rjust(15,'0') #003 A 017 - CPF/CNPJ fornecedor
+            linha << I18n.transliterate(item_repasse.cartorio.dados_cartorario.nome)[0,30].gsub('º',' ').gsub('°',' ').upcase.ljust(30) #018 A 047 - Nome do fornecedor
+            linha << I18n.transliterate(item_repasse.cartorio.dados_cartorario.endereco)[0,40].gsub('º',' ').gsub('°',' ').upcase.ljust(40,' ') # 048 A 087 Endereco do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.cep.gsub('.','').gsub('-','') #088 A 095 -  Cep do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.codigo_banco.rjust(3,'0') #096 A 098 - Codigo do banco do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.codigo_agencia.rjust(5,'0') # 099 A 103 - Codigo da agencia do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.digito_agencia.ljust(1,' ') # 104 A 104 - digito da agencia do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.conta_corrente.rjust(13,'0') # 105 A 117 - conta ocrrente do fornecedor
+            linha << item_repasse.cartorio.dados_cartorario.digito_conta_corrente.ljust(2,' ') #118 A 119 - digito da conta corrente do fornecedor
+            linha << ItemRepasse.numero_pagamento.to_s.ljust(16,' ') #120 A 135 - Número do Pagamento.
+            linha << '000' #136 A 138 - Carteira - fixo 000
+            linha << ''.rjust(12,'0') #139 A 150 - Nosso numero - fixo 0
+            linha << ''.rjust(15,'0') #151 A 165 - Seu numero - fixo 0
+            linha << item_repasse.repasse.data_vencimento.strftime('%Y%m%d').rjust(8,'0') #166 A 173 # data de vencimento
+            linha << ''.rjust(8,'0') #174 A 181 # data de emissao - fixo 0
+            linha << ''.rjust(8,'0') #182 A 189 # data limite para desconto - fixo 0
+            linha << '0' #190 A 190 - Fixo 0
+            linha << '0'.rjust(4,'0') #191 A 194 - Fator de vencimento - fixo 0
+            linha << '0'.rjust(10,'0') #195 A 204 - Valor do Documento - fixo 0
+            linha << item_repasse.valor.contabil.gsub('.','').gsub(',','').rjust(15,'0') # 205 A 219 # valor do pagamento
+            linha << '0'.rjust(15,'0') #220 A 234 - Valor do desconto - fixo 0
+            linha << '0'.rjust(15,'0') #235 A 249 - Valor do acrescimo - fixo 0 
+            linha << '05' #250 A 251 - Tipo de documento - 05 = OUTROS
+            linha << '0'.rjust(10,'0') #252 A 261 - Numero nota fiscal - fixo 0
+            linha << '  ' #262 A 263 - serie do documento - opicional 
+            linha << (item_repasse.cartorio.dados_cartorario.codigo_banco == '237' ? '05' : '03' ) #264 A 265 - modalidade de pagamento - 01 = transf, 03 = doc
+            linha << item_repasse.repasse.data_vencimento.strftime('%Y%m%d').rjust(8,"0") #266 A 273 # data para efetivacao do pagmento
+            linha << ''.ljust(3,' ') # 274 A 276 - Moeda - Fixo Branco
+            linha << '01' # 277 a 278 - Situação do Agendamento - Fixo 01
+            linha << ''.ljust(10,' ') #279 A 288 - Informacao de retorno - fixo branco
+            linha << '0' # 289 A 289 - Tipo de movimento - 0 = inclusao
+            linha << '25' # 290 A 291 - Codigo do movimento - 25 = autoriza agendamento
+            linha << ''.ljust(4,' ') # 292 A 295 - Horario para consulta de saldo - fixo branco
+            linha << ''.ljust(15,' ')# 296 A 310 - Saldo disponivel - fixo branco
+            linha << ''.ljust(15,' ')# 311 A 325 - Valor da taxa pre-funding - fixo branco  
+            linha << ''.ljust(6,' ') # 326 A 331 - fixo brancos
+            linha << ''.ljust(40,' ') # 332 A 371 - Sacador avalista  somente para titulos em cobranca - fixo brancos
+            linha << ' '#372 A 372 - Fixo Branco
+            linha << ' '#373 A 373 - Nivel da informacao do retorno - Fixo Branco
+            linha << informacoes_complementares #374 a 413 # informacoes complementares
+            linha << '00' #414 A 415 - codigo de area na empresa - opcional
+            linha << ''.ljust(35,' ') # 416 A 450 - Para uso da empresa - fixo branco
+            linha << ''.ljust(22,' ') # 451 A 472 - Fixo Brancos
+            linha << (item_repasse.id - 40000).to_s.rjust(5,'0')   #473 A 477 - Codigo de lancamento
+            linha << ' ' # 478 A 478 reserva - fixo branco
+            linha << item_repasse.cartorio.dados_cartorario.tipo_conta.to_s #479 A 479 - Tipo de conta do fornecedor - 1 = conta corrente, 2= conta poupanca
+            linha << self.parametros.conta.rjust(7,'0') #480 A 486 - Conta complementar - Fixo 0
+            linha << ''.ljust(8,' ') # 487 A 494 - fixo branco
+            linha << numero_de_registros.to_s.rjust(6,'0')  # 495 a 500 num sequencial do registro
+            linha << "\n"
+            @arquivo.write(linha)
+          end
+        end
+        return numero_de_registros
+      end
+
     
       def rodape_arquivo_remessa(numero_de_registros)
         numero_de_registros += 1
