194a1fcaaad9a8cf3521d0cf41568856f56e8155
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2016-01-13 13:20:16 -0300

    Update rghost.rb

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -214,7 +214,7 @@ module Brcobranca
           doc.text_in :write => boleto.nosso_numero_boleto,     :x => "18.1 cm",  :y => "19.42 cm", :tag => :negrito  
 
           # Linha 2
-          doc.text_in :write => boleto.numero_documento,                            :x => "0.7 cm",   :y => "18.23 cm", :tag => :negrito  
+          doc.text_in :write => boleto.numero_documento,                            :x => "0.7 cm",   :y => "20.23 cm", :tag => :negrito  
           doc.text_in :write => "#{boleto.documento_cedente.formata_documento}",    :x => "8.39 cm",  :y => "18.23 cm", :tag => :negrito  
           doc.text_in :write => boleto.data_vencimento.to_s_br,                     :x => "12.15 cm", :y => "18.23 cm", :tag => :negrito  
           doc.text_area "<negrito>#{boleto.valor_documento.to_currency}</negrito>", :x => "13.68 cm", :y => "18.23 cm", :text_align => :right, :width => "6.83 cm"
