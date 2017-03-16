dae0cb722229aab054bc1cadb199758c24eea123
daniellvieira <danielvieiraufc@gmail.com>
2017-03-16 15:20:15 -0300

    alteracao cedente 2

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -138,8 +138,8 @@ module Brcobranca
           doc.show "#{boleto.banco}-#{boleto.banco_dv}", tag: :grande
           doc.moveto x: '7.5 cm', y: '23.9 cm'
           doc.show boleto.codigo_barras.linha_digitavel, tag: :grande
-          doc.moveto x: '1.5 cm', y: '24.45 cm', :width => "8 cm"
-          doc.show boleto.cedente
+          doc.moveto x: '1.5 cm', y: '23.05 cm'
+          doc.text_area "#{boleto.cedente}", :x => "1.5 cm",   :y => "23.45 cm", :width => "8 cm" 
           doc.moveto x: '11 cm', y: '23 cm'
           doc.show boleto.agencia_conta_boleto
           doc.moveto x: '14.2 cm', y: '23 cm'
