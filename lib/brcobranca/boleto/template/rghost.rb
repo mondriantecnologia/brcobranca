e6cf7a7d435483001988296062d9d92af16fd592
Mondrian Tecnologia <suporte.mondrian@gmail.com>
2017-03-16 14:25:32 -0300

    layout3

--- lib/brcobranca/boleto/template/rghost.rb
+++ lib/brcobranca/boleto/template/rghost.rb
@@ -296,13 +296,13 @@ module Brcobranca
           doc.show "#{boleto.banco}-#{boleto.banco_dv}", tag: :grande
           doc.moveto x: '7.5 cm', y: '23.9 cm'
           doc.show boleto.codigo_barras.linha_digitavel, tag: :grande
-          doc.moveto x: '0.7 cm', y: '23.0 cm'
-          doc.text_area "#{boleto.cedente}", :x => "0.7 cm",   :y => "23.1 cm", :width => "10 cm" 
-          doc.moveto x: '10 cm', y: '23.1 cm'
+          doc.moveto x: '0.7 cm', y: '23.05 cm'
+          doc.text_area "#{boleto.cedente}", :x => "0.7 cm",   :y => "23.05 cm", :width => "10 cm" 
+          doc.moveto x: '10 cm', y: '23.05 cm'
           doc.show boleto.agencia_conta_boleto
-          doc.moveto x: '14.2 cm', y: '23.1 cm'
+          doc.moveto x: '14.2 cm', y: '23.05 cm'
           doc.show boleto.especie
-          doc.moveto x: '15.7 cm', y: '23.1 cm'
+          doc.moveto x: '15.7 cm', y: '23.05 cm'
           doc.show boleto.quantidade
           doc.moveto x: '0.7 cm', y: '22.2 cm'
           doc.show boleto.numero_documento
@@ -310,7 +310,7 @@ module Brcobranca
           doc.show boleto.documento_cedente.formata_documento.to_s
           doc.moveto x: '12 cm', y: '22.2 cm'
           doc.show boleto.data_vencimento.to_s_br
-          doc.moveto x: '20.3 cm', y: '23 cm'
+          doc.moveto x: '20.3 cm', y: '23.05 cm'
           doc.show boleto.nosso_numero_boleto, align: :show_right
           doc.moveto x: '20.3 cm', y: '22.2 cm'
           doc.show boleto.valor_documento.to_currency, align: :show_right
